76ecdfb342621582c87124d0520012d480d005e5
kdum <57634850+kdum@users.noreply.github.com>
2020-05-16 20:58:05 +0300

    Updates

---
+++ precache-manifest.0cd910aa457978201bf916ec048e441b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6024d208f03b3a930056b694e4b7e07d",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "55049814f7d6492bfdc3",
+    "url": "/tic-tac-toe/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "64a7194dc9131ee1433a",
+    "url": "/tic-tac-toe/static/js/2.3947d38d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.3947d38d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55049814f7d6492bfdc3",
+    "url": "/tic-tac-toe/static/js/main.fab0ce3d.chunk.js"
+  },
+  {
+    "revision": "67093bcd19aa82d2fa67",
+    "url": "/tic-tac-toe/static/js/runtime-main.29e8e2be.js"
+  }
+]);
